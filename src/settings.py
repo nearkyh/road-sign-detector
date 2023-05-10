e6f56b1d9aaf032c68450cb1acbc77670287ec4c
Yonghan Kim <nearkyh@gmail.com>
2023-05-10 23:33:48 +0900

    Add train device setting module

--- src/settings.py
+++ src/settings.py
@@ -15,3 +15,15 @@ def set_seed(seed, multi_gpu=False):
             torch.cuda.manual_seed_all(seed)
         torch.backends.cudnn.benchmark = False
         torch.backends.cudnn.deterministic = True
+
+
+def set_device():
+    if torch.cuda.is_available():
+        device = torch.device('cuda:0')
+        device_name = torch.cuda.get_device_name(device)
+        print(f'Using GPU: {device_name}')
+    else:
+        device = torch.device('cpu')
+        print('Using CPU')
+
+    return device
